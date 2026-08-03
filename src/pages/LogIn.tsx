b2c55734a4cfa620e0dc215cfd160b71673e65f8
mohammad hader <m7mad7ader@gmail.com>
2026-08-03 20:42:55 +0300

    LogIn Page Component

---
+++ src/pages/LogIn.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const LogIn = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit =(e)=>{
+    e.preventDefault();
+    // logic here
+    console.log("Email:", email, "Password:", password);
+    }
+
+
+  return (
+<div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="card shadow p-4" style={{ width: "350px" }}>
+        <h3 className="text-center mb-4">University Registration Login</h3>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="student@university.edu"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100">
+            Login
+          </button>
+        </form>
+
+        <div className="mt-3 text-center">
+          <a href="/forgot-password" className="text-decoration-none">
+            Forgot Password?
+          </a>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default LogIn
