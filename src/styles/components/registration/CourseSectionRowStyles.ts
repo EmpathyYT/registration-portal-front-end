73ebc8461b26528903ede4834e8b5eb460afc542
Ammar SA <ammar.ahmad.sameed@gmail.com>
2026-08-28 17:59:22 +0300

    de

--- src/styles/components/registration/CourseSectionRowStyles.ts
+++
@@ -1,5 +0,0 @@
-export const styles = {
-    addBtn: 'btn btn-outline-success btn-sm fw-bold shadow-sm rounded-3 px-3',
-    courseId: 'text-secondary',
-    courseName: 'fw-semibold',
-};
--- src/styles/components/registration/RegisteredCourseRowStyles.ts
+++
@@ -1,5 +0,0 @@
-export const styles = {
-    dropBtn: 'btn btn-outline-danger btn-sm fw-bold shadow-sm rounded-3 px-3',
-    courseId: 'text-secondary',
-    courseName: 'fw-semibold',
-};
